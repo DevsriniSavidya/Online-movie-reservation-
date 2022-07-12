374bf20b369e64765ca18e5ab561a8e017ef702e
Piumika Saranga <IT20260460@my.sliit.lk>
2022-07-13 00:33:40 +0530

    summery

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import React from 'react'
-import AddMovie from './Components/AddMovie'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import DisplayMovie from './Components/DisplayMovie';
-import MovieDetails from './Components/MovieDetails';
-import UpdateMovie from './Components/UpdateMovie';
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {DataProvider} from './GlobalState';
+import Header from './components/headers/Header.js';
+import Pages from './components/mainpages/Pages';
 
 function App() {
   return (
-     <Router>
-         <Switch>
-          <Route exact path="/"><AddMovie/></Route>
-          <Route  path="/display"><DisplayMovie/></Route>
-           <Route  path="/movieDetails"><MovieDetails/></Route>
-           <Route  path="/updateMovie"><UpdateMovie/></Route>
-          
-          
-         </Switch>
+    <DataProvider>
+      <Router>
+          <Header />
+          <Pages/>
       </Router>
-  )
+    </DataProvider>
+  );
 }
 
 export default App;
